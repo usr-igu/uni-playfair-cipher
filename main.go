ce28d34915201b7080b8b9469866d19ee3e3b558
Igor Breno Estácio Dutra de Oliveira <igorbreno95@outlook.com>
2017-06-18 00:33:39 -0400

    Playfair está +/-, vou tentar debugar agora.

--- main.go
+++ main.go
@@ -2,22 +2,59 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"strings"
 )
 
 // Playfair ...
 func Playfair(msg, keyword string) string {
-	fmt.Printf("Message to cipher: %s, Keyword: %s\n", msg, keyword)
+
+	msg = processMsg(msg)
+	encodedMsg := ""
+
+	fmt.Printf("Message to cipher: %s (%d), Keyword: %s\n", msg, len(msg), keyword)
 
 	table := createTable(keyword)
+
+	// TODO: Simplificar essa função D :
+	for i := 0; i < len(msg); i += 2 {
+
+		c, cn := rune(msg[i]), rune(msg[i+1])
+
+		row, col := whereInTheTable(c, table)
+		rown, coln := whereInTheTable(cn, table)
+
+		if row == rown {
+			if row <= 4 {
+				encodedMsg += string(table[row][col+1])
+				encodedMsg += string(table[rown][coln+1])
+			} else {
+				encodedMsg += string(table[row][4-col+1])
+				encodedMsg += string(table[rown][4-coln+1])
+			}
+		} else if col == coln {
+			if col <= 4 {
+				encodedMsg += string(table[row+1][col])
+				encodedMsg += string(table[rown+1][coln])
+			} else {
+				encodedMsg += string(table[4-row+1][col])
+				encodedMsg += string(table[4-rown+1][coln])
+			}
+		} else {
+			dist := abs(col - coln)
+			encodedMsg += string(table[row][abs(dist-col)])
+			encodedMsg += string(table[rown][abs(dist-coln)])
+		}
+
+	}
+
 	printTable(table)
 
-	fmt.Println(processMsg(msg))
-
-	return ""
+	return encodedMsg
 }
 
 // processMsg ...
+// TODO: Essa função não está funcionando para todos os casos.
 func processMsg(msg string) string {
 	// FIXME: Acho que isso aqui é meio ineficiente rsrs
 	msg = strings.Join(strings.Split(strings.ToUpper(msg), " "), "")
@@ -36,6 +73,10 @@ func processMsg(msg string) string {
 		} else {
 			processedMsg += string(a) + "X" + string(b)
 		}
+	}
+
+	if len(processedMsg)%2 != 0 {
+		processedMsg = processedMsg[:len(processedMsg)-1]
 	}
 
 	return processedMsg
@@ -117,6 +158,11 @@ func whereInTheTable(c rune, table [5][5]rune) (int, int) {
 		}
 	}
 	return x, y
+}
+
+// abs utility
+func abs(x int) int {
+	return int(math.Abs(float64(x)))
 }
 
 func main() {
